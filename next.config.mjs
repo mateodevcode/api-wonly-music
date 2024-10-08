087365528531b8d0908f02533c21e12ce8e8abdc
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-10-08 10:20:51 +0200

    Se actualiza el archivo next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,13 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/artistas",
+        source: "/api/:artistas*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET",
           },
           {
             key: "Access-Control-Allow-Headers",
