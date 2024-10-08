e898c77607b294310c7717471d40c0f108aba737
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-10-08 10:23:55 +0200

    Se configuro el archivo next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/:artistas*",
+        source: "/api/artistas",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
